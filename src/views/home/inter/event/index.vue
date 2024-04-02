9f0f9e5ccce524e9c400966c2ffdf8f1d401ee29
kejiawei <>
2024-04-02 16:05:19 +0800

    事件页面

--- src/views/home/inter/event/index.vue
+++ src/views/home/inter/event/index.vue
@@ -1,6 +1,22 @@
 <template>
-    <div>
-event
+    <div class="event">
+        <div class="left">
+            输入
+            <div class="table_out">
+                <div class="icon">
+                </div>
+                <div class="table_in">
+                </div>
+            </div>
+        </div>
+        <div class="right">
+            输出
+            <div>
+                <div class="table_out">
+                
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,5 +25,10 @@ event
 </script>
 
 <style lang="scss" scoped>
-
+    .event{
+        display: flex;
+        .left{
+            flex:0.5
+        }
+    }
 </style>
