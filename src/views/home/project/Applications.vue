d63c8957cff5a5bc02968f8dcca64920c2a803a7
jasonwang1816 <wang.yu.you@foxmail.com>
2024-05-24 11:20:15 +0800

    增加路由页面

---
+++ src/views/home/project/Applications.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Application</div>
+</template>
+
+<script setup>
+</script>
+
+<style   scoped>
+</style>
---
+++ src/views/home/project/Deploymentplan.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>部署方案</div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
