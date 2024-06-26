948414e5347b9e2a6cf7bee44eff6af9359a0af4
kejiawei <>
2024-06-26 13:35:15 +0800

    动态得到系统变量

--- src/api/systeminter/systemvari/index.ts
+++ src/api/systeminter/systemvari/index.ts
@@ -14,42 +14,54 @@ export const getSystemOutputVaris=(project,modeule,id)=>{
  * @param id NODEID，用来取内置变量
  */
 const getSystemVaris=(project,modeule,type,id)=>{
-    getSegChildrenIds(project,modeule,id);
-    // //得到配置列表
-    // let configList = cache.local.getJSON(cacheKey_configList);
-    // let deployment = cache.local.getJSON(deploymentCacheKey)?.nodes;
-    // configList=configList.filter(x=>x.type==type);
-    // let rltList=new Array();
-    //
-    // //把配置列表里的变量加上
-    // if (configList||configList.length>0){
-    //     configList.forEach((config)=>{
-    //         //得到设备
-    //         let currentDeployment=deployment.find(x=>x.id==config.nodeId);
-    //         rltList.push({
-    //             key:config.id,
-    //             //为防止变量冲突，加上设备前缀
-    //             text:currentDeployment?.label+"_"+config.name,
-    //             type:getType(config.ioType)
-    //         })
-    //     })
-    // }
-    // //这里需要把 write/read转化成XML里的对应字符串作为参数，再去得到内置变量
-    // rltList.concat(getInsideVari(project ,module,convertTypeToXmlType(type),id));
-    // //把内置变量给加上
-    // return rltList;
+    let segChildrenIds=getSegChildrenIds(project,modeule,id);
+    if (!segChildrenIds||segChildrenIds.length<=0){
+        return ;
+    }
+    //得到配置列表
+    let configList = cache.local.getJSON(cacheKey_configList);
+    let deployment = cache.local.getJSON(deploymentCacheKey)?.nodes;
+    //得到对应ID的配置列表
+    configList=configList.filter(x=>x.type==type&&segChildrenIds.includes(x.nodeId)&&x.isGroup!=1);
+    if (!configList||configList.length<=0){
+        return ;
+    }
+    let rltList=new Array();
+    //把配置列表里的变量加上
+    configList.forEach((config)=>{
+        //得到设备
+        let currentDeployment=deployment.find(x=>x.id==config.nodeId);
+        rltList.push({
+            key:config.id,
+            //为防止变量冲突，加上设备前缀
+            text:currentDeployment?.label+"_"+config.name,
+            type:getType(config.ioType)
+        })
+    })
+    //这里需要把 write/read转化成XML里的对应字符串作为参数，再去得到内置变量
+    rltList.concat(getInsideVari(project ,module,convertTypeToXmlType(type),id));
+    //把内置变量给加上
+    return rltList;
 }
 //通过网络段ID查找他下面设备的所有ID
 const getSegChildrenIds=(project,modeule,segId)=>{
+    //查找该网络段所关联的所有线
     let deploymentEdges = cache.local.getJSON(deploymentCacheKey)?.edges;
     if (!deploymentEdges){
         return;
     }
-    let relatgedIds=new Array();
-    console.log(deploymentEdges,segId)
-    relatgedIds.push(deploymentEdges.filter(x=>x.source==segId).map(x=>x.target));
-    relatgedIds.push(deploymentEdges.filter(x=>x.target==segId).map(x=>x.source));
-    console.log(relatgedIds)
+    //由于可能是双向，把target和source的都加上
+    let relatgedIds=deploymentEdges.filter(x=>x.source==segId).map(x=>x.target);
+    if (!relatgedIds){
+        relatgedIds=new Array();
+    }
+    relatgedIds.concat(deploymentEdges.filter(x=>x.target==segId).map(x=>x.source));
+    if (relatgedIds.length<=0){
+        return;
+    }
+    //在nodes里查找出所有cardInfo.largeType=target_device的ID
+    let deploymentNodes = cache.local.getJSON(deploymentCacheKey)?.nodes;
+    return deploymentNodes.filter(x=>relatgedIds.includes(x.id)&&x.cardInfo.largeType=="target_device")?.map(x=>x.id);
 }
 
 /**
