96b4a5560718a10c4fbbaef3257497beec6479ab
jasonwang1816 <wang.yu.you@foxmail.com>
2024-06-28 13:56:01 +0800

    修改控制器资源点表

--- src/components/pointTable/ModbusTable.vue
+++ src/components/pointTable/ModbusTable.vue
@@ -1,9 +1,13 @@
 <template>
-  <el-table :data="modelValue.tableData" style="width: 100%">
+  <el-table id="modtb" :data="modelValue.tableData" style="width: 100%">
     <el-table-column label="序号" type="index" width="80" />
     <el-table-column prop="registerAddress" label="寄存器地址" width="180" />
     <el-table-column prop="variable" label="变量" width="180" />
-    <el-table-column prop="variableName" label="变量名称" width="180" />
+    <el-table-column prop="variableName" label="变量名称" width="180">
+      <template #default="{ row }">
+        <el-input v-model="row.variableName" size="small"></el-input>
+      </template>
+    </el-table-column>
     <el-table-column prop="variableType" label="变量类型" width="180" />
     <el-table-column prop="dataType" label="数据类型" width="180" />
     <el-table-column label="操作" width="180">
@@ -23,5 +27,8 @@ const { modelValue } = defineProps({
 });
 </script>
 
-<style  scoped>
+<style  >
+#modtb .el-input__wrapper {
+  box-shadow: 0 0 0 1px #444549 inset;
+}
 </style>
--- src/components/pointTable/OPCUA_RES.vue
+++ src/components/pointTable/OPCUA_RES.vue
@@ -6,6 +6,7 @@
     top="2vh"
     v-model="modelValue.status"
     show-close
+    :before-close="beforeClose"
     :close-on-click-modal="true"
     :close-on-press-escape="false"
   >
@@ -16,7 +17,12 @@
     </template>
     <el-form :inline="true" :model="formInline" class="demo-form-inline">
       <el-form-item label="节点类型">
-        <el-input v-model="formInline.user" placeholder="节点类型" clearable />
+        <el-select placeholder="请选择节点类型" v-model="formInline.user">
+          <el-option label="请选择" value="" key="请选择" />
+          <el-option label="只读" value="只读" key="只读" />
+          <el-option label="关闭" value="关闭" key="关闭" />
+          <el-option label="可读可写" value="可读可写" key="可读可写" />
+        </el-select>
       </el-form-item>
       <el-form-item label="应用程序">
         <el-select v-model="formInline.region" placeholder="应用程序" clearable>
@@ -36,12 +42,21 @@
       </el-form-item>
     </el-form>
     <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="date" label="序号" type="index" width="80" />
-      <el-table-column prop="date" label="节点号" width="180" />
-      <el-table-column prop="name" label="节点名称" width="180" />
-      <el-table-column prop="address" label="节点类型" width="180">
+      <el-table-column label="序号" type="index" width="80" />
+      <el-table-column prop="nodeNo" label="节点号" width="180" />
+      <el-table-column prop="nodeName" label="节点名称" width="180">
         <template #default="{ row }">
-          <el-select placeholder="请选择节点类型" size="small">
+          <el-input v-model="row.nodeName" size="small"></el-input>
+        </template>
+      </el-table-column>
+      <el-table-column prop="nodeType" label="节点类型" width="180">
+        <template #default="{ row }">
+          <el-select
+            placeholder="请选择节点类型"
+            v-model="row.nodeType"
+            size="small"
+          >
+            <el-option label="请选择" value="" key="请选择" />
             <el-option label="只读" value="只读" key="只读" />
             <el-option label="关闭" value="关闭" key="关闭" />
             <el-option label="可读可写" value="可读可写" key="可读可写" />
@@ -49,16 +64,19 @@
         </template>
       </el-table-column>
 
-      <el-table-column prop="address" label="控制器资源" width="180" />
-      <el-table-column prop="address" label="应用程序" width="180" />
-      <el-table-column prop="address" label="功能块名称" width="180" />
-      <el-table-column prop="address" label="变量名称" width="180" />
-      <el-table-column prop="address" label="变量类型" width="180" />
+      <el-table-column prop="resourceName" label="控制器资源" width="180" />
+      <el-table-column prop="appName" label="应用程序" width="180" />
+      <el-table-column prop="funcName" label="功能块名称" width="180" />
+      <el-table-column prop="varName" label="变量名称" width="180" />
+      <el-table-column prop="varType" label="变量类型" width="180" />
     </el-table>
   </el-dialog>
 </template>
 
 <script setup>
+import cache from "@/plugins/cache.ts";
+import { onMounted } from "vue";
+let cacheKey = "functionBlock";
 const { modelValue } = defineProps({
   modelValue: {
     type: Object,
@@ -74,7 +92,72 @@ const formInline = reactive({
 
 let tableData = ref([]);
 const onSubmit = () => {
-  console.log("submit!");
+  //console.log("submit!", tableData.value);
+};
+
+onMounted(() => {
+  //console.log("onMounted ", modelValue.returnValue);
+  let jsonFuncBlock = cache.local.getJSON(cacheKey);
+  if (jsonFuncBlock) {
+    let nodes = jsonFuncBlock.nodes;
+    if (nodes) {
+      let iNo = 50216;
+      //console.log("nodes===", nodes);
+      nodes.forEach((e) => {
+        e.inputVar.forEach((i) => {
+          let varType = "输入";
+          let varName = i.name;
+          assembleTable(e, iNo, varType, varName);
+          iNo++;
+        });
+
+        e.outputVar.forEach((i) => {
+          let varType = "输出";
+          let varName = i.name;
+          assembleTable(e, iNo, varType, varName);
+          iNo++;
+        });
+      });
+    }
+  }
+});
+
+const assembleTable = (e, iNo, varType, varName) => {
+  let appName = e.title;
+  let funcName = e.label;
+  let resourceName = e?.selectedResource?.label;
+  let nodeName = `${appName}_${funcName}_${varName}`;
+  let nodeType = "";
+
+  let existObj = modelValue.returnValue.find(
+    (x) =>
+      x.appName == appName &&
+      x.resourceName == resourceName &&
+      x.funcName == funcName &&
+      x.varType == varType &&
+      x.varName == varName
+  );
+  if (existObj) {
+    nodeName = existObj.nodeName;
+    nodeType = existObj.nodeType;
+  }
+  tableData.value.push({
+    nodeNo: iNo,
+    nodeName,
+    nodeType,
+    resourceName,
+    appName,
+    funcName,
+    varName,
+    varType,
+  });
+};
+
+const beforeClose = (done) => {
+  modelValue.returnValue = tableData.value;
+  console.log("done::", done, modelValue.returnValue);
+  done();
+  //modelValue.status = false;
 };
 </script>
 
@@ -83,5 +166,9 @@ const onSubmit = () => {
 <style>
 #opcua_resDig .el-dialog__body {
   padding-top: 5px !important;
+}
+
+#opcua_resDig .el-table .el-input__wrapper {
+  box-shadow: 0 0 0 1px #444549 inset;
 }
 </style>
